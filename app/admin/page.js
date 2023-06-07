db5092ead9d13d65e545a5b06de372ea3599623c
Mem0Real <juniourmimo@gmail.com>
2023-06-07 04:22:12 +0300

    Used fetch for list items inside admin(trial2)

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,13 +4,14 @@ import Actions from "./Actions";
 export default async function AdminPage() {
   const res = await getEverything();
 
+  const data = res.json();
   return (
     <div className="flex flex-col w-full justify-center items-center bg-neutral-100 text-neutral-900 md:mt-6">
       <h1 className="text-lg underline underline-offset-2 shadow-inner shadow-black px-5 py-2 rounded-md md:mb-6">
         Admin Page
       </h1>
 
-      <Actions data={res} />
+      <Actions data={data} />
     </div>
   );
 }
