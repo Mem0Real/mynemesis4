3188d751579d1bc80724718336d92c6c3870e937
Mem0Real <juniourmimo@gmail.com>
2023-06-07 04:20:24 +0300

    Used fetch for list items inside admin

---
+++ libraries/getEverything_fetch.js
@@ -0,0 +1,21 @@
+export default async function getEverything() {
+  const env = process.env.NODE_ENV;
+  let url;
+
+  if (env === "development") url = "http://localhost:3001/api/getAll";
+  else if (env === "production") url = "https://mynemesis4-mem0real.vercel.app";
+
+  const res = await fetch(url, {
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok) {
+    return undefined;
+  }
+
+  const data = await res.json();
+
+  return data;
+}
